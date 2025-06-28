b1fa7197e2e5dbe8ef7972b88ece1724ec621f9f
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-06-28 23:14:30 +0200

    Reduced Models to only Product (let's take single-step approach), changed Database to PostGreSQL, added basic templates and styling

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Product, ProductType
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductType)
--- project/settings.py
+++ project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pancernik_db',
+        'USER': 'pancernik_user',
+        'PASSWORD': os.getenv('POSTGRES_DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -118,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
