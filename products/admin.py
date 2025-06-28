eac4bef2082dfdc970976b8c367535c85fe33ba4
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-06-29 00:37:09 +0200

    Made sidebar and nav sticky, made new migrations and added Admin Models

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Product, ProductType
 
-admin.site.register(Product)
-admin.site.register(ProductType)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'product_type')
+    search_fields = ('code', 'name')
+    list_filter = ('product_type',)
+
+
+@admin.register(ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
