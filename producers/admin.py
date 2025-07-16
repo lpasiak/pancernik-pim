ceea08a183b9c7c0210500f303c746bf8f018d70
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-07-16 16:07:45 +0200

    Added Producer Admin

--- producers/admin.py
+++ producers/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+from .models import Producer
+
+
+@admin.register(Producer)
+class ProducerAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name', )
+    list_filter = ('name', )
